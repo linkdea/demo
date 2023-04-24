da101213b26dc886be4c86144594f6f77f7cec06
Yuan-Yen Tai <anrris330@gmail.com>
2023-04-24 14:12:04 -0700

    Changes to be committed: 	modified:   js/wmdict.js

--- js/wmdict.js
+++ js/wmdict.js
@@ -26,7 +26,7 @@ wmdict = {
     "audi.1": {
         "title": "Audi e-tron",
         "description": "Audi e-tron named 2023 World Car of the Year, thanks to its exceptional performance, sustainable design, and advanced technology. With a range of up to 222 miles on a single charge and innovative features like a digital virtual cockpit, the e-tron is leading the way in the electric vehicle market.",
-        "src": "../creatives/audi.webp",
+        "src": "creatives/audi.webp",
         "url_learn": "https://www.audi.com/en.html",
         "url_app": "https://apps.apple.com/us/app/audi-on-demand-car-rental/id587128247",
         "url_map": "https://maps.apple.com/?address=3350%20Stevens%20Creek%20Blvd,%20San%20Jose,%20CA%20%2095117,%20United%20States&auid=9647188736749467794&ll=37.322755,-121.956218&lsp=9902&q=Audi%20Stevens%20Creek"
@@ -34,7 +34,7 @@ wmdict = {
     "nike.1": {
         "title": "Nike AirSport",
         "description": "Introducing Nike AirSport: The Ultimate Athletic Shoe for Maximum Performance: Nike has launched its latest innovation in athletic footwear with the new Nike AirSport. This revolutionary shoe is designed to enhance athletic performance with its advanced technology and stylish design, making it the go-to shoe for athletes of all levels..",
-        "src": "../creatives/nike.webp",
+        "src": "creatives/nike.webp",
         "url_learn": "https://www.nike.com/",
         "url_app": "https://apps.apple.com/us/app/nike-shoes-apparel-stories/id1095459556",
         "url_map": "https://maps.apple.com/?address=377%20Santana%20Row,%20San%20Jose,%20CA%2095128,%20United%20States&auid=3832468590474063611&ll=37.322470,-121.948099&lsp=9902&q=Nike"
