6828a31d90b112a5908a12714aa930c3784d99e7
Yuan-Yen Tai <anrris330@gmail.com>
2023-04-24 13:52:02 -0700

    Changes to be committed: 	new file:   .DS_Store 	new file:   Blog-Template.css 	new file:   Demo.css 	new file:   Demo.html 	new file:   Home.css 	new file:   Home.html 	new file:   Post-Template.css 	new file:   app-template/bootstrap.min.css 	new file:   app-template/index.html 	new file:   creatives/audi.webp 	new file:   creatives/nike.webp 	new file:   data/Test_Podcast_Short.tsv 	new file:   data/the_other_side.json 	new file:   data/wm_cta.json 	new file:   images/0fd3416c.jpeg 	new file:   images/68f64b9d.jpeg 	new file:   images/8ad73f3c.jpeg 	new file:   images/Romney_cosmic_audio-interference_stargazing_34679ab4-2a7e-41d4-9f1c-a6b46627b3e4.png 	new file:   images/sound-wave-with-imitation-sound-audio-identification-technology_106065-64.png 	new file:   index.html 	new file:   intlTelInput/intlTelInput.css 	new file:   intlTelInput/intlTelInput.min.js 	new file:   intlTelInput/utils.js 	new file:   jquery.js 	new file:   js/aic_wasm_decoder.js 	new file:   js/aic_wasm_decoder.wasm 	new file:   js/decode_handler.js 	new file:   js/decoder.js 	new file:   nicepage.css 	new file:   nicepage.js

---
+++ js/decode_handler.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var wmdict = undefined;
+fetch('../data/wm_cta.json')
+.then((response) => response.json())
+.then((json) => {
+    wmdict = json;
+});
+
+var wm_title = document.getElementById('wm_title');
+var wm_description = document.getElementById('wm_description');
+var wm_creatives = document.getElementById('wm_creatives');
+var wm_link = document.getElementById('wm_link');
+var wm_app = document.getElementById('wm_app');
+var wm_map = document.getElementById('wm_map');
+
+wm_link.style.display = 'none';
+wm_app.style.display = 'none';
+wm_map.style.display = 'none';
+
+var is_first_time = true;
+
+linkdea_wm_decoder((decoded) => {
+
+    function updatePage(wmmeta){
+        wm_title.innerHTML = wmmeta['title'];
+        wm_description.innerHTML = wmmeta['description'];
+        wm_creatives.style.display = (wmmeta['src'] === "") ? 'none' : 'block';
+
+        wm_link.style.display =(wmmeta['url_learn'] === "#") ? 'none' : 'block';
+        wm_app.style.display = (wmmeta['url_app'] === "#") ? 'none' : 'block';
+        wm_map.style.display = (wmmeta['url_map'] === "#") ? 'none' : 'block';
+
+        wm_creatives.src = wmmeta['src'];
+        wm_link.innerHTML =`<a href="${wmmeta['url_learn']}" >Learn More</a>`;
+        wm_app.innerHTML = `<a href="${wmmeta['url_app']}" >App Store</a>`;
+        wm_map.innerHTML = `<a href="${wmmeta['url_map']}" >Apple Map</a>`;
+    }
+
+    if (is_first_time) {
+        is_first_time = false;
+        let localObj = localStorage.getItem("saved_wm_meta")
+        if (localObj !== null){
+            wmmeta = JSON.parse(localObj);
+            updatePage(wmmeta);
+        }
+    }
+
+    if (decoded in wmdict){
+        var wmmeta = wmdict[decoded];
+        updatePage(wmmeta);
+        localStorage.setItem("saved_wm_meta", JSON.stringify(wmmeta));
+    }
+
+});
---
+++ js/decoder.js
@@ -0,0 +1,92 @@
+'use strict';
+
+window.ga =
+    window.ga ||
+    function () {
+        (ga.q = ga.q || []).push(arguments);
+    };
+ga("create", "UA-33848682-1", "auto");
+ga("set", "transport", "beacon");
+ga("send", "pageview");
+
+function linkdea_wm_decoder(decode_handler){
+    Module.onRuntimeInitialized = function() {    // Make sure EMSCRIPTEN_BINDINGS are called before we try to use them
+
+        var audioSelect = document.querySelector('select#audioSource');
+        audioSelect.onchange = getStream;
+
+        getStream().then(getDevices).then(gotDevices);
+
+        function getDevices() {
+            // AFAICT in Safari this only gets default devices until gUM is called :/
+            return navigator.mediaDevices.enumerateDevices();
+        }
+
+        function gotDevices(deviceInfos) {
+            window.deviceInfos = deviceInfos; // make available to console
+            console.log('Available input and output devices:', deviceInfos);
+            for (const deviceInfo of deviceInfos) {
+                const option = document.createElement('option');
+                option.value = deviceInfo.deviceId;
+                if (deviceInfo.kind === 'audioinput') {
+                    option.text = deviceInfo.label || `Microphone ${audioSelect.length + 1}`;
+                    audioSelect.appendChild(option);
+                }
+            }
+        }
+
+        function handleError(error) {
+            console.error('Error: ', error);
+        }
+
+        function getStream() {
+            if (window.stream) {
+                window.stream.getTracks().forEach(track => {
+                    track.stop();
+                });
+            }
+            const audioSource = audioSelect.value;
+            const constraints = {
+                audio: {deviceId: audioSource ? {exact: audioSource} : undefined}
+            };
+            return navigator.mediaDevices.getUserMedia(constraints).
+                then(processStream).catch(handleError);
+        }
+
+        function processStream(stream) {
+            window.stream = stream; // make stream available to console
+            audioSelect.selectedIndex = [...audioSelect.options].
+                findIndex(option => option.text === stream.getAudioTracks()[0].label);
+        
+            var audioContext;
+            var bufferLength = 1024;
+            let sampleRate = 24000;
+            var audioContext = new (window.AudioContext || window.webkitAudioContext)({sampleRate: sampleRate});
+            var analyser = audioContext.createAnalyser();
+            audioContext.createMediaStreamSource(stream).connect(analyser);
+            var dataArray = new Float32Array(bufferLength);
+
+            var sampleBuilder = new Module.SampleBuilder(sampleRate, bufferLength);
+            var freqBands = new Module.vector_float();
+            freqBands.push_back(2000);
+            for(let i=0; i<12; i++) freqBands.push_back(200);
+            var segmentDecoder = new Module.SegmentDecoder(sampleRate, 3, 100, 0, freqBands, 11, 10, 100)
+        
+            function update() {
+                analyser.getFloatTimeDomainData(dataArray);
+                for (let i=sampleBuilder.buffer_tail_index(dataArray[0], dataArray[1], dataArray[2]); i<dataArray.length; i++){
+                    sampleBuilder.push_sample(dataArray[i]);
+                    if(segmentDecoder.decode_sample(dataArray[i])){
+                        var decoded = segmentDecoder.decoded();
+                        if (decoded.length > 0) {
+                            decode_handler(decoded);
+                        }
+                    }
+                }
+                requestAnimationFrame(update);
+            }
+            update();
+        }
+
+    };
+}
