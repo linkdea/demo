a99affacf84049d1b8499d95529469ee687d9b8e
Yuan-Yen Tai <anrris330@gmail.com>
2023-04-24 21:40:38 -0700

    Changes to be committed: 	modified:   js/decoder.js 	modified:   js/wmdict.js

--- js/wmdict.js
+++ js/wmdict.js
@@ -38,6 +38,61 @@ wmdict = {
         "url_learn": "https://www.nike.com/",
         "url_app": "https://apps.apple.com/us/app/nike-shoes-apparel-stories/id1095459556",
         "url_map": "https://maps.apple.com/?address=377%20Santana%20Row,%20San%20Jose,%20CA%2095128,%20United%20States&auid=3832468590474063611&ll=37.322470,-121.948099&lsp=9902&q=Nike"
-
+    },
+    "000001": {
+        "title": "Chapter 1: Proposition",
+        "description": "Utterance starts: Can I buy you a drink? \nUtterance ends: Sounds thrilling, doesn't it?",
+        "src": "",
+        "url_learn": "#",
+        "url_app": "#",
+        "url_map": "#"
+    },
+    "000002": {
+        "title": "Chapter 2: The Cost of Freedom",
+        "description": "Utterance starts: Let's just say that I find it hard to get a job. \nUtterance ends: You'd be risking everything, but on the other hand, you just might find yourself a free man.",
+        "src": "",
+        "url_learn": "#",
+        "url_app": "#",
+        "url_map": "#"
+    },
+    "000003": {
+        "title": "Chapter 3: The Invitation",
+        "description": "Utterance starts: Right here, right now, I put the offer out. \nUtterance ends: It'll take you to the other side.",
+        "src": "",
+        "url_learn": "#",
+        "url_app": "#",
+        "url_map": "#"
+    },
+    "000004": {
+        "title": "Chapter 4: Resistance and Contentment",
+        "description": "Utterance starts: Okay, my friend, you want to cut me in.\n Utterance ends: I don't need to see the other side.",
+        "src": "",
+        "url_learn": "#",
+        "url_app": "#",
+        "url_map": "#"
+    },
+    "000005": {
+        "title": "Chapter 5: Debating the Decision",
+        "description": "Utterance starts: Now is this really how you like to spend your days? \nUtterance ends: But I guess I'll leave that up to you.",
+        "src": "",
+        "url_learn": "#",
+        "url_app": "#",
+        "url_map": "#"
+    },
+    "000006": {
+        "title": "Chapter 6: Negotiating the Deal",
+        "description": "Utterance starts: Well, it's intriguing, but to go would cost me greatly. \nUtterance ends: Sir, looks like you have yourself a junior partner.",
+        "src": "../creatives/hired.png",
+        "url_learn": "https://hired.com/",
+        "url_app": "#",
+        "url_map": "#"
+    },
+    "000007": {
+        "title": "Chapter 7: Embracing the Adventure",
+        "description": "Utterance starts: What I have is an overcompensated apprentice. \nUtterance ends: We're going to the other side.",
+        "src": "",
+        "url_learn": "#",
+        "url_app": "#",
+        "url_map": "#"
     }
 }
